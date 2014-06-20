a831004d1efab4e62798707d1862eb5f0981fba2
brittany <b.cooperbc@gmail.com>
2014-06-20 07:50:13 -0400

    splash-page edits

--- mail.php
+++ mail.php
@@ -2,18 +2,16 @@
 $action=$_REQUEST['action']; 
 if ($action=="")               /* send the submitted data */ 
     { 
-    $name=$_REQUEST['name']; 
     $email=$_REQUEST['email']; 
-    $message=$_REQUEST['message']; 
-    if (($name=="")||($email=="")||($message=="")) 
+    if ($email=="")
         { 
-        echo "All fields are required, please fill <a href=\"\">the form</a> again."; 
+        echo "All fields are required, please fill in<a href=\"\">your email</a> again."; 
         } 
     else{         
         $from="From: $name<$email>\r\nReturn-path: $email"; 
-        $subject="Message sent using your contact form"; 
+        $subject="New email submitted on yunite.co"; 
         mail("b.cooperbc@gmail.com", $subject, $message, $from); 
-        echo "Email sent!"; 
+        echo "Thank You!"; 
         } 
     }   
 ?> 
